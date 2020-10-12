e46995d3da808ff4ea2a5543e19a5a2a2e3eacef
Chris Wein <chriswein12@yahoo.com>
2020-10-11 21:36:25 -0500

    able to populate search history city list

--- script.js
+++ script.js
@@ -1,17 +1,57 @@
 var apiKey = "2e1d35c035f3e53a227b30889160b667";
-var cities = [];
 
-var citySearchFormEl = document.querySelector("#city-search-form")
+var citySearchFormEl = document.querySelector("#city-search-form");
 var citySearchInputEl = document.querySelector("#search-input");
-var citySearchBtnEl = document.querySelector("#search");
+var citySearchBtnEl = document.querySelector("#search-button");
+var historyEl = document.querySelector("#city-list");
+
+function displayCities() {
+    cities = JSON.parse(localStorage.getItem("cities")) || [];
+    cities.reverse();
+    console.log(cities);
+
+    var limit;
+
+    if (cities.length < 10) {
+        limit = cities.length;
+    } else {
+        limit = 10;
+    }
+
+    for (i = 0; i < limit; i++) {
+        var cityListEl = document.createElement("button");
+        cityListEl.classList = "list-group-item list"
+        cityListEl.id = "city-button-" + i;
+        cityListEl.textContent = cities[i];
+
+        historyEl.appendChild(cityListEl);
+
+
+    }
+
+
+}
 
 var citySelector = function(event) {
+    debugger;
     event.preventDefault();
 
     var cityName = citySearchInputEl.value.trim();
 
+
     if (cityName) {
-        console.log(cityName);
+        // todaysWeather(cityName);
+        // weeklyWeather(cityName);
+        var cities = JSON.parse(localStorage.getItem("cities")) || [];
+
+       if (!cities.includes(cityName)) {
+       cities.push(cityName);
+       console.log(cities);
+       localStorage.setItem("cities", JSON.stringify(cities));
+       displayCities();  
+       }
+    } else {
+        alert("Please enter a city name");
     }
 }
 
@@ -19,5 +59,7 @@ var citySelector = function(event) {
 
 
 
+
+displayCities();
 
 citySearchFormEl.addEventListener("submit", citySelector);
