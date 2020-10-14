28e14ee922b1d48e516989c14f55c5815340271b
Chris Wein <chriswein12@yahoo.com>
2020-10-14 04:51:11 -0500

    Added comments to HTML and JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var apiKey = "2e1d35c035f3e53a227b30889160b667";
 
+// Variables for page elements
 var citySearchFormEl = document.querySelector("#city-search-form");
 var citySearchInputEl = document.querySelector("#search-input");
 var citySearchBtnEl = document.querySelector("#search-button");
@@ -7,6 +8,7 @@ var historyEl = document.querySelector("#city-list");
 var todaysWeatherEl = document.querySelector("#todays-weather");
 var weeklyWeatherEl = document.querySelector("#weekly-weather");
 
+// Function used to display the search history, up to 10 cities
 function displayCities() {
     historyEl.textContent = "";
 
@@ -32,24 +34,28 @@ function displayCities() {
     }
 }
 
+// jQuery function used to make search history clickable
 $("#city-list").on("click", "button", function() {
     var selectCity = $(this).text();
     console.log(selectCity);
     todaysWeather(selectCity);
     weeklyWeather(selectCity);
-
 })
 
+// function to enter in a city name into the search
 var citySelector = function(event) {
     event.preventDefault();
 
     var cityName = citySearchInputEl.value.trim();
 
-
+    // checks if a city name has been entered
     if (cityName) {
         var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial";
+        // this fetch is just for checking to see if we get a valid response from the api before continuing.
         fetch(apiUrl).then(function(response) {
             if (response.ok) {
+
+                // if reponse is okay, the two main functions are called and the search is saved to local storage
                 todaysWeather(cityName);
                 weeklyWeather(cityName);
 
@@ -64,7 +70,6 @@ var citySelector = function(event) {
             } else {
                 alert("Unable to obtain weather for that city. Please check spelling of city.");
             }
-
         })
         .catch(function(error) {
             alert("Unable to connect to weather service at this time")
@@ -74,6 +79,7 @@ var citySelector = function(event) {
     }
 }
 
+// first main function to pull up todays weather using the openweathermap API
 var todaysWeather = function (city) {
     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
     
@@ -86,6 +92,7 @@ var todaysWeather = function (city) {
     });
 }
 
+// function to display today's weather once data is received
 function displayTodaysWeather (weatherData) {
     todaysWeatherEl.classList = "border border-dark p-2 rounded-lg"
     var [month, date, year] = ( new Date() ).toLocaleDateString().split("/");
@@ -96,27 +103,32 @@ function displayTodaysWeather (weatherData) {
     var lat = weatherData.coord.lat
     var lon = weatherData.coord.lon
 
+    // creating date
     var titleEl = document.createElement("h2");
     titleEl.textContent = weatherData.name + " (" + month + "/" + date + "/" + year + ")";
     var iconEl = document.createElement("img");
     iconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png")
 
+    // creating temp
     var temperatureEl = document.createElement("p");
     temperatureEl.textContent = "Temperature: " + Math.round(weatherData.main.temp) + " °F"
 
+    // creating humidity
     var humidityEl = document.createElement("p");
     humidityEl.textContent = "Humidity: " + weatherData.main.humidity + "%";
 
+    // creating windspeed
     var windSpeedEl = document.createElement("p");
     windSpeedEl.textContent = "Wind Speed: " + Math.round(weatherData.wind.speed) + " MPH";
 
-
+    // appending data
     titleEl.append(iconEl);
     todaysWeatherEl.appendChild(titleEl);
     todaysWeatherEl.appendChild(temperatureEl);
     todaysWeatherEl.appendChild(humidityEl);
     todaysWeatherEl.appendChild(windSpeedEl);
 
+    // function to display UV Index
     var uvData = function(data) {
         console.log(data.value);
 
@@ -132,9 +144,9 @@ function displayTodaysWeather (weatherData) {
         }
     
         todaysWeatherEl.appendChild(uvIndexEl);
-
     }
     
+    // fetch to receive UV Index data
     fetch("http://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon+"&appid="+apiKey)
     .then(function(response){
         response.json().then(function(data) {
@@ -143,6 +155,7 @@ function displayTodaysWeather (weatherData) {
     }); 
 }
 
+// second main function to pull 5 day forecast info
 var weeklyWeather = function(city) {
    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
 
@@ -155,6 +168,7 @@ var weeklyWeather = function(city) {
     });
 }
 
+// function to display 5 day forecast
 function displayWeeklyWeather (weeklyData) {
     console.log(weeklyData);
 
@@ -202,13 +216,8 @@ function displayWeeklyWeather (weeklyData) {
             weeklyCardHolderEl.appendChild(weeklyColEl);
         }
     }
-
     weeklyWeatherEl.appendChild(weeklyCardHolderEl);
-
-
 }
-
-
 
 displayCities();
 
